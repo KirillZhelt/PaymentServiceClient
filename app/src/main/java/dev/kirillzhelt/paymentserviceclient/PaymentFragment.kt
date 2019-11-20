3b75f651f701af2c7c682f7ef0464dd3e0759d6e
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-11-20 22:26:54 +0300

    Pick dates from material components calendar

--- app/src/main/java/dev/kirillzhelt/paymentserviceclient/PaymentFragment.kt
+++ app/src/main/java/dev/kirillzhelt/paymentserviceclient/PaymentFragment.kt
@@ -9,11 +9,28 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.lifecycle.Observer
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog
 
 
 class PaymentFragment : Fragment() {
 
     private lateinit var viewModel: PaymentViewModel
+
+    private lateinit var serviceNameEditText: EditText
+    private lateinit var methodNameEditText: EditText
+
+    private lateinit var chooseDateFromButton: Button
+    private lateinit var dateFromTextView: TextView
+    private lateinit var dateFromPickerDialog: DatePickerDialog
+
+    private lateinit var chooseDateToButton: Button
+    private lateinit var dateToTextView: TextView
+    private lateinit var dateToPickerDialog: DatePickerDialog
+
+    private lateinit var payButton: Button
+
+    private lateinit var resultTextView: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,25 +38,57 @@ class PaymentFragment : Fragment() {
     ): View? {
         val inflatedView = inflater.inflate(R.layout.fragment_payment, container, false)
 
-        val serviceNameEditText: EditText = inflatedView.findViewById(R.id.fragment_payment_service_name_et)
-        val methodNameEditText: EditText = inflatedView.findViewById(R.id.fragment_payment_method_name_et)
+        serviceNameEditText = inflatedView.findViewById(R.id.fragment_payment_service_name_et)
+        methodNameEditText = inflatedView.findViewById(R.id.fragment_payment_method_name_et)
 
-        val chooseDateFromButton: Button = inflatedView.findViewById(R.id.fragment_payment_choose_date_from_btn)
-        val chooseDateToButton: Button = inflatedView.findViewById(R.id.fragment_payment_choose_date_to_btn)
+        chooseDateFromButton = inflatedView.findViewById(R.id.fragment_payment_choose_date_from_btn)
+        chooseDateToButton = inflatedView.findViewById(R.id.fragment_payment_choose_date_to_btn)
 
-        val payButton: Button = inflatedView.findViewById(R.id.fragment_payment_pay_btn)
+        dateFromTextView = inflatedView.findViewById(R.id.fragment_payment_date_from_tv)
+        dateToTextView = inflatedView.findViewById(R.id.fragment_payment_date_to_tv)
 
-        val resultTextView: TextView = inflatedView.findViewById(R.id.fragment_payment_result_tv)
+        payButton = inflatedView.findViewById(R.id.fragment_payment_pay_btn)
 
-
+        resultTextView = inflatedView.findViewById(R.id.fragment_payment_result_tv)
 
         return inflatedView
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
         viewModel = ViewModelProviders.of(this).get(PaymentViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        dateFromPickerDialog = createDatePickerDialog(viewModel::onDateFromPicked)
+        dateToPickerDialog = createDatePickerDialog(viewModel::onDateToPicked)
+
+        viewModel.dateFrom.observe(this, Observer { date ->
+            dateFromTextView.text = date
+        })
+
+        viewModel.dateTo.observe(this, Observer { date ->
+            dateToTextView.text = date
+        })
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        chooseDateFromButton.setOnClickListener {
+            dateFromPickerDialog.show(fragmentManager!!, "DateFromPickerDialog")
+        }
+
+        chooseDateToButton.setOnClickListener {
+            dateToPickerDialog.show(fragmentManager!!, "DateToPickerDialog")
+        }
+    }
+
+    private fun createDatePickerDialog(onDatePicked: (Int, Int, Int) -> Unit): DatePickerDialog {
+        return DatePickerDialog.newInstance { _, year, monthOfYear, dayOfMonth ->
+            onDatePicked(year, monthOfYear, dayOfMonth)
+        }.apply {
+            isThemeDark = false
+            showYearPickerFirst(false)
+        }
+    }
 }
--- app/src/main/java/dev/kirillzhelt/paymentserviceclient/PaymentViewModel.kt
+++ app/src/main/java/dev/kirillzhelt/paymentserviceclient/PaymentViewModel.kt
@@ -1,7 +1,33 @@
 package dev.kirillzhelt.paymentserviceclient
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class PaymentViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _dateFrom = MutableLiveData<LocalDate>()
+    val dateFrom: LiveData<String> = Transformations.map(_dateFrom) {
+        it.format(FORMATTER)
+    }
+
+    private val _dateTo = MutableLiveData<LocalDate>()
+    val dateTo: LiveData<String> = Transformations.map(_dateTo) {
+        it.format(FORMATTER)
+    }
+
+    fun onDateFromPicked(year: Int, month: Int, day: Int) {
+        _dateFrom.value = LocalDate.of(year, month, day)
+    }
+
+    fun onDateToPicked(year: Int, month: Int, day: Int) {
+        _dateTo.value = LocalDate.of(year, month, day)
+    }
+
+    companion object {
+        val FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy")
+    }
 }
