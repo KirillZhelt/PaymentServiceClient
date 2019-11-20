589d57e5f6208395dec75745743e6cd5682ff20c
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-11-20 21:45:43 +0300

    Create layout

--- app/src/main/java/dev/kirillzhelt/paymentserviceclient/PaymentFragment.kt
+++ app/src/main/java/dev/kirillzhelt/paymentserviceclient/PaymentFragment.kt
@@ -6,13 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 
 class PaymentFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = PaymentFragment()
-    }
 
     private lateinit var viewModel: PaymentViewModel
 
@@ -20,7 +19,21 @@ class PaymentFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_payment, container, false)
+        val inflatedView = inflater.inflate(R.layout.fragment_payment, container, false)
+
+        val serviceNameEditText: EditText = inflatedView.findViewById(R.id.fragment_payment_service_name_et)
+        val methodNameEditText: EditText = inflatedView.findViewById(R.id.fragment_payment_method_name_et)
+
+        val chooseDateFromButton: Button = inflatedView.findViewById(R.id.fragment_payment_choose_date_from_btn)
+        val chooseDateToButton: Button = inflatedView.findViewById(R.id.fragment_payment_choose_date_to_btn)
+
+        val payButton: Button = inflatedView.findViewById(R.id.fragment_payment_pay_btn)
+
+        val resultTextView: TextView = inflatedView.findViewById(R.id.fragment_payment_result_tv)
+
+
+
+        return inflatedView
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
